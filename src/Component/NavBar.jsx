16c6439a28a152ee0dd53c128c5cc420b7bc40e9
meganarmas <meganarmas@gmail.com>
2025-01-13 11:00:54 -0600

    fixed the navbar

--- src/Component/NavBar.jsx
+++ src/Component/NavBar.jsx
@@ -1,22 +1,51 @@
+import React from "react";
 import { Link } from "react-router-dom";
 import { Navbar, Nav, Container } from "react-bootstrap";
-import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function NavigationBar() {
-    return(
-        <Navbar className="p-2 mb-3" bg="light">
-            <Navbar.Brand as={Link} className="navbar-brand-custom" to="/home">Home</Navbar.Brand> <br />
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Brand as={Link} className="navbar-brand-custom" to="/login">Login</Navbar.Brand> <br />
-            <Navbar.Brand as={Link} className="navbar-brand-custom" to="/logout">Logout</Navbar.Brand> <br />
-            <Navbar.Brand as={Link} className="navbar-brand-custom" to="/shopping-cart">Shopping Cart</Navbar.Brand> <br />
-            <Navbar.Brand as={Link} className="navbar-brand-custom" to="/products">Products</Navbar.Brand> <br />
-            <Navbar.Brand as={Link} className="navbar-brand-custom" to="/catalog">Product Catalog</Navbar.Brand> <br />
-            <Navbar.Brand as={Link} className="navbar-brand-custom" to="/order-history">Order History</Navbar.Brand> <br />
-            <Navbar.Brand as={Link} className="navbar-brand-custom" to="/create">Register</Navbar.Brand> <br />
-            <Navbar.Brand as={Link} className="navbar-brand-custom" to="/update">Update Account</Navbar.Brand> <br />
-            <Navbar.Brand as={Link} className="navbar-brand-custom" to="/delete">Delete Account</Navbar.Brand> <br />
+    return (
+        <Navbar expand="lg" className="p-2" style={{ width: "100%", backgroundColor: "beige" }}>
+            <Container fluid>
+                <Navbar.Brand as={Link} to="/home" className="fw-bold text-uppercase">
+                   E-Commerce App
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbar-nav" />
+                <Navbar.Collapse id="navbar-nav">
+                    <Nav className="ms-auto">
+                        <Nav.Link as={Link} to="/" className="nav-item-custom">
+                            Home
+                        </Nav.Link>
+                        <Nav.Link as={Link} to="/login" className="nav-item-custom">
+                            Login
+                        </Nav.Link>
+                        <Nav.Link as={Link} to="/logout" className="nav-item-custom">
+                            Logout
+                        </Nav.Link>
+                        <Nav.Link as={Link} to="/shopping-cart" className="nav-item-custom">
+                            Shopping Cart
+                        </Nav.Link>
+                        <Nav.Link as={Link} to="/products" className="nav-item-custom">
+                            Products
+                        </Nav.Link>
+                        <Nav.Link as={Link} to="/catalog" className="nav-item-custom">
+                            Product Catalog
+                        </Nav.Link>
+                        <Nav.Link as={Link} to="/order-history" className="nav-item-custom">
+                            Order History
+                        </Nav.Link>
+                        <Nav.Link as={Link} to="/create" className="nav-item-custom">
+                            Register
+                        </Nav.Link>
+                        <Nav.Link as={Link} to="/update" className="nav-item-custom">
+                            Update Account
+                        </Nav.Link>
+                        <Nav.Link as={Link} to="/delete" className="nav-item-custom">
+                            Delete Account
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
         </Navbar>
     );
 }
